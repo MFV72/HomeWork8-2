510a3f6695510c4a9e4dff6e24dd12c01a52384e
mfv <mit.fedor@gmail.com>
2020-09-25 19:44:45 +0500

    was added test for methods volume and station

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
 public class Radio {
-    private int station = 0;
-    private int volume = 5;
+    private int station;
+    private int volume;
 
     public void increaseStation() {
         if (station < 9) {
@@ -12,17 +12,23 @@ public class Radio {
         }
     }
 
+       public void decreaseStation() {
+        if (station>0) {
+            station--;
+        } else {
+            station = 9;
+        }
+    }
+
     public void increaseVolume() {
         if (volume < 10) {
             volume++;
         }
     }
 
-    public void decreaseStation() {
-        if (station>0) {
-            station--;
-        } else {
-            station = 9;
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
         }
     }
 
@@ -31,9 +37,10 @@ public class Radio {
     }
 
     public void setVolume(int volume) {
-        if ((volume>=0)&(volume<=10)) {
-            this.volume = volume;
+        if ((volume < 0) | (volume >10)) {
+           return;
         }
+        this.volume = volume;
     }
 
     public int getStation() {
@@ -41,9 +48,10 @@ public class Radio {
     }
 
     public void setStation(int station) {
-        if ((station >=0)&(station<=9)) {
-            this.station = station;
+        if ((station < 0) | (station > 9)) {
+            return;
         }
+        this.station = station;
     }
 
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,28 +1,107 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-    @Test
-    void increaseStation() {
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','6'",
+                    "'8','9'",
+                    "'9','0'",
+                    "'0','1'"},
+            delimiter = ','
+    )
+    void shouldIncreaseStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(currentStation);
+        radio.increaseStation();
+        int actual = radio.getStation();
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void increaseVolume() {
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','4'",
+                    "'1','0'",
+                    "'0','9'",
+                    "'9','8'"},
+            delimiter = ','
+    )
+    void shouldDecreaseStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(currentStation);
+        radio.decreaseStation();
+        int actual = radio.getStation();
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void decreaseStation() {
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','6'",
+                    "'9','10'",
+                    "'10','10'",
+                    "'0','1'"},
+            delimiter = ','
+    )
+
+    void shouldIncreaseVolume(int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(currentVolume);
+        radio.increaseVolume();
+        int actual = radio.getVolume();
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void setVolume() {
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','4'",
+                    "'1','0'",
+                    "'0','0'",
+                    "'10','9'"},
+            delimiter = ','
+    )
+    void shouldDecreaseVolume(int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(currentVolume);
+        radio.decreaseVolume();
+        int actual = radio.getVolume();
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void setStation() {
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','5'",
+                    "'0', '0'",
+                    "'-1','0'",
+                    "'10','10'",
+                    "'11','0'"}, delimiter = ','
+    )
+    void shouldSetVolume(int newVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(newVolume);
+        int actual = radio.getVolume();
+        assertEquals(expected, actual);
     }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'5','5'",
+                    "'0', '0'",
+                    "'-1','0'",
+                    "'9','9'",
+                    "'10','0'"}, delimiter = ','
+    )
+    void shouldSetStation(int newStation, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(newStation);
+        int actual = radio.getStation();
+        assertEquals(expected, actual);
+    }
+
+
 }
